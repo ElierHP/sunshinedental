03c16ef8c8ca41030b39731bef1702a02ec5a1c1
Elier H <elierporto@gmail.com>
2020-12-02 17:37:41 -0500

    Initial commit from gatsby: (https://github.com/Nomad-Freedom/gatsby-starter.git)

---
+++ src/components/section/layout.js
@@ -0,0 +1,50 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import theme from "../../css/theme"
+import { css, Global } from "@emotion/core"
+
+import Header from "./header"
+import "./layout.css"
+import Footer from "./Footer"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Global
+        styles={css`
+          div {
+            background: ${theme.colors.background};
+            color: ${theme.colors.text};
+          }
+        `}
+      />
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
